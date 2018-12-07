62d24f100257a9c5af35c311a34f4c802feccd54
Spartak <bugdaryan@gmail.com>
2018-12-07 04:04:52 +0400

    Add user validations Add callback to normalize email Add has_secure_password, password_confirmation Add user routes Add user crud Add jwt helper functions Add jwt encode decode functionality Add seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,45 @@
 class ApplicationController < ActionController::API
     require 'json_web_token'
+
+    protected
+    attr_reader :current_user
+
+    def authenticate_request!
+        if !payload || !JsonWebToken.valid_payload?(payload.first)
+            return invalid_authentication
+        end
+
+        load_current_user!
+        invalid_authentication unless @current_user
+    end
+
+    def invalid_authentication
+        render json: {error: "Invalid Request"}, status: :unauthorized
+    end
+
+    def admin_only
+        invalid_authentication unless payload[0]['aud'] == 'admin'
+    end
+
+    def f_manager_only
+        invalid_authentication unless payload[0]['aud'] == 'f_manager' || invalid_authentication unless payload[0]['aud'] == 'admin'
+    end
+
+    def l_manager_only
+        invalid_authentication unless payload[0]['aud'] == 'l_manager' || invalid_authentication unless payload[0]['aud'] == 'admin'
+    end
+
+    private
+
+    def payload
+        auth_header = request.headers['Authorization']
+        token = auth_header.split(' ').last
+        JsonWebToken.decode(token)
+    rescue
+        nil
+    end
+
+    def load_current_user!
+        @current_user = User.find_by(id: payload[0]['user_id'])
+    end
 end
