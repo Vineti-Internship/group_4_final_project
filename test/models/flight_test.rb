bfabaa6d306ac6b294ba1856d346397a3db8da90
arpi <arpiarustamyan21@gmail.com>
2018-12-08 23:26:57 +0400

    feat(flight): added test and validation

---
+++ test/models/flight_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+describe Flight, type: :model do
+  describe 'associations' do
+    describe 'belongs_to' do
+      it { is_expected.to belong_to(:lane).inverse_of(:flights) }
+    end
+
+    describe 'belongs_to' do
+      it { is_expected.to belong_to(:airplane).inverse_of(:flights) }
+    end
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:from) }
+    it { is_expected.to validate_presence_of(:to) }
+    it { is_expected.to validate_presence_of(:flight_start) }
+    it { is_expected.to validate_presence_of(:flight_time) }
+  end
+end
