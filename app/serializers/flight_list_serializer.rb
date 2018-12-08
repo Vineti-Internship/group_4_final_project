022b3242947c3f49f1e5f3ff6d39b40b87622915
arpi <arpiarustamyan21@gmail.com>
2018-12-08 23:58:00 +0400

    feat(serializer): added flight and flight list serializers

---
+++ app/serializers/flight_list_serializer.rb
@@ -0,0 +1,7 @@
+class FlightListSerializer < ActiveModel::Serializer
+  attributes :from, :to, :flight_start, :flight_time , :airplane_name
+  
+  def airplane_name
+    object.airplane.name
+  end
+end
