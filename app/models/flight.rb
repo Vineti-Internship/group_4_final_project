e415459a27610a07d77f2d547f03b7ae7bd53847
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-09 02:29:02 +0400

    feat(associations): added many to one connection added has_many/has_one through association

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :lane, inverse_of: :flights
   belongs_to :airplane, inverse_of: :flights
+  has_many :tickets
+  has_many :users, through: :tickets
 
   validates :from, :to, :flight_start, :flight_time, :lane, :airplane, presence: true
 end
