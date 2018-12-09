0d4f6b4816160a5c618e9b7cd99ce2320f24cc2f
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-10 00:06:12 +0400

    feat(test): add ticket model test fix user model test indent minor fix lane_max_counts.yml

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid ticket' do
+    ticket = Ticket.new(user_id:User.first.id, flight_id:Flight.first.id)
+    assert ticket.valid?
+  end
+
+  test 'invalid without user' do
+    ticket = Ticket.new(flight_id:Flight.first.id)
+    refute ticket.valid?, 'ticket is valid without user'
+    assert_not_nil ticket.errors[:user_id], 'no validation error for user_id'
+  end
+
+  test 'invalid without flight' do
+    ticket = Ticket.new(user_id:User.first.id)
+    refute ticket.valid?, 'ticket is valid without flight'
+    assert_not_nil ticket.errors[:flight_id], 'no validation error for flight_id'
+  end
+
 end
