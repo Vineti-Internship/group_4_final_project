1d21d09537d4aa60c12e91f5f9bc3129d6dca063
Armine Gevorgyan <arminegevorgyan1993@gmail.com>
2018-12-09 16:49:58 +0400

    feat: add some validations

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -1,3 +1,5 @@
 class Airplane < ApplicationRecord
   belongs_to :airline
+  has_one :flight
+  validates :name, :model, :status, :country, :time_on_lane, presence: true
 end
--- app/serializers/flight_list_serializer.rb
+++ app/serializers/flight_list_serializer.rb
@@ -1,5 +1,5 @@
 class FlightListSerializer < ActiveModel::Serializer
-  attributes :from, :to, :flight_start, :flight_time , :airplane_name
+  attributes :from, :to, :flight_start, :flight_time, :airplane_name
   
   def airplane_name
     object.airplane.name
