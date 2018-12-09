def424e05c8334dcb4e99d5bf3e4f6f2cb6bd6b9
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-09 20:26:50 +0400

    feat(controller): add ticket controller minor fix: add dependency to airplane minor typo : in user model feat(route): add tickets route fix seeds, added flights seed

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,40 @@
+class TicketsController < ApplicationController
+    before_action :set_ticket, only: [:show, :destroy]
+    before_action :authenticate_request!
+    before_action :flight_manager_only, only: :index
+
+    def index
+        tickets = Ticket.all
+        render json: tickets
+    end
+
+    def show
+        render json: @ticket
+    end
+
+    def create
+        ticket = Ticket.new (user_id: current_user.id, ticket_params)
+        if ticket.save
+            render json: {status: 'ticket created successfully'}, status: :created
+        else
+            render json: {errors: ticket.errors.full_messages}, status: :bad_request
+        end
+    end
+
+    def destroy
+        if @ticket.destroy
+            head(:ok)
+        else
+            head(:unprocessable_entity)
+        end
+    end
+
+    private
+    def set_ticket
+        @ticket = Ticket.find(params[:id])
+    end
+
+    def ticket_params
+        params.require(:ticket).permit(:flight_id)
+    end
+end
