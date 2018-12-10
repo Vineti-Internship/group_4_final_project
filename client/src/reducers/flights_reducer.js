f1b81273921b064aaed0e2e555143a2d6fb9d232
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-11 03:40:45 +0400

    feat(action): add flight action add case in flight reducer for getFlights case

--- client/src/reducers/flights_reducer.js
+++ client/src/reducers/flights_reducer.js
@@ -4,6 +4,7 @@ const initialState = [];
 
 export default (flights = initialState, action) => {
 	switch(action.type){
-	
+		case actionTypes.getFlights:
+			return [...flights, action.payload];
 	}
 };
