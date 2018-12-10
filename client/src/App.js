114a88a62119aa8bd8dcbdb3af09e995ca8aee04
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-11 03:09:01 +0400

    add redux/react-redux add flights component created folder structure initial setup : add empty store, flights reducer,action types add basic route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
+import {BrowserRouter as Router, Link, Redirect, Route} from "react-router-dom";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-      
+				<div className='nav'>
+					
+				</div>
+				<Router>
+					<switch>
+						<Route exact path = '/flights' render ={()=> <Flights/>} />
+						<Route exact path = '/' render={()=>  <Redirect to='/flights' />} />
+					</switch>
+				</Router>
 			</div>
 		);
 	}
