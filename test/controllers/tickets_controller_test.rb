565ca9f723d6e3009175396e804eb0a10eae7ea6
Spartak <bugdaryan@gmail.com>
2018-12-11 02:19:02 +0400

    feat(test): add tickets controller test fix users controller test minor fix: user controller

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @ticket = tickets(:one)
+  end
+
+  test "shouldn't get index" do
+    get tickets_url, as: :json
+    assert_response :unauthorized
+  end
+
+  test "shouldn't create a ticket" do
+    assert_difference('Ticket.count', 0) do
+      post tickets_url, params: { ticket: { flight_id: Flight.first.id} }, as: :json
+    end
+
+    assert_response 401
+  end
+
+  test "shouldn't show a ticket" do
+    get ticket_url(@ticket), as: :json
+    assert_response :unauthorized
+  end
+
+  test "shouldn't destroy ticket" do
+    assert_difference('Ticket.count', 0) do
+      delete ticket_url(@ticket), as: :json
+    end
+
+    assert_response 401
+  end
 end
