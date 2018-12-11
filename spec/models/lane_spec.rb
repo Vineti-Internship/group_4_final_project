109b21c526628ca71025b26a7b98b925709ec787
arpi <arpiarustamyan21@gmail.com>
2018-12-12 00:49:23 +0400

    fix(spec): fix lane spec

--- spec/models/lane_spec.rb
+++ spec/models/lane_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Lane, :type => :model do
   describe "Associations" do
     it { should have_many(:flights) }
-    it { should belong_to(:lane_max_count) }
   end
   describe "Validations" do 
     it { should validate_presence_of(:capacity) }
