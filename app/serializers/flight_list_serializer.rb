1a6a942f109af6fdf9b19dc4e63e5c490a5533ed
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-12 02:56:24 +0400

    fix: serializers

--- app/serializers/flight_list_serializer.rb
+++ app/serializers/flight_list_serializer.rb
@@ -1,7 +1,9 @@
 class FlightListSerializer < ActiveModel::Serializer
-  attributes :from, :to, :flight_start, :flight_time, :airplane_name
+  attributes :id, :from, :to, :flight_start, :flight_time, :airline_name
+  belongs_to :airplane
+  belongs_to :lane
   
-  def airplane_name
-    object.airplane.name
+  def airline_name
+    object.airplane.airline.name
   end
 end
