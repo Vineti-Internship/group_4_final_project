22fe81a9f283c74b92adadd14e8e9c328325187e
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-11 20:38:02 +0400

    add capacity migration fix eslint error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_152426) do
+ActiveRecord::Schema.define(version: 2018_12_11_163515) do
 
   create_table "airlines", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_152426) do
     t.integer "airline_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "capacity"
     t.index ["airline_id"], name: "index_airplanes_on_airline_id"
   end
 
