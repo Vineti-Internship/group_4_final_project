2ce2ea71540123780e6462ed9825e982d4884e6e
Spartak Bughdaryan <spartakbughdaryan@Spartaks-MacBook-Air.local>
2018-12-12 03:25:31 +0400

    feat(table): add flights table minor change: changed time_on_lane to integer add flight_end to flight serializer fix: seeds

--- client/src/components/Flights/Flights.js
+++ client/src/components/Flights/Flights.js
@@ -4,9 +4,30 @@ class Flights extends React.Component {
 	render() {
 		return (
 			<div className="flights">
-				{this.props.flights && this.props.flights.map(flight => {
-					return <h2 key={flight.id}>{flight.from}-{flight.to}</h2>;
-				})}
+				<table>
+					<tbody>
+						<tr>
+							<th>Flight No</th>
+							<th>Origination airport</th>
+							<th>Airline</th>
+							<th>Scheduled departure</th>
+							<th>Scheduled arrival</th>
+						</tr>
+						{this.props.flights.map(flight => {
+							return (
+								<tr key={flight.id}>
+									<th>{flight.id}</th>
+									<th>{flight.to}</th>
+									<th>{flight.airline_name}</th>
+									<th>{flight.flight_start}</th>
+									<th>{flight.flight_end}</th>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+
+
 				<button onClick={this.props.handleClick}>CLick me</button>
 			</div>
 		);
