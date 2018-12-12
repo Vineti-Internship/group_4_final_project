721d0599c9bab1542e5d6f1a10848ccb11150d67
arpi <arpiarustamyan21@gmail.com>
2018-12-12 04:30:47 +0400

    fix(serializer): fix flight end in flight and flight list serializers

--- app/serializers/flight_list_serializer.rb
+++ app/serializers/flight_list_serializer.rb
@@ -1,12 +1,13 @@
 class FlightListSerializer < ActiveModel::Serializer
-  attributes :id, :from, :to, :flight_start, :flight_end, :airline_name
   belongs_to :airplane
   belongs_to :lane
 
+  attributes :id, :from, :to, :flight_start, :flight_end, :airline_name
+
   def flight_end 
-    object.flight_start+30.minutes
+    object.flight_start + (object.flight_time.hour).hours + (object.flight_time.min).minutes
   end
-  
+
   def airline_name
     object.airplane.airline.name
   end
