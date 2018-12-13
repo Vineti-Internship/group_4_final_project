50c32ec7cc4eb7c5e91e988ec1fdc445f86b25e7
Spartak <bugdaryan@gmail.com>
2018-12-13 14:49:07 +0400

    fix: airplanes actions fix combine reducers

--- client/src/actions/airplanes_actions.js
+++ client/src/actions/airplanes_actions.js
@@ -2,14 +2,20 @@ import * as actionTypes from "./types";
 import axios from "axios";
 
 export const findAirplanes = (data) => async dispatch => {
-	const res = await axios.post("/airplanes/find", {...data});
-	const airplanes = await res.data;
-	dispatch(getFoundAirplanes(airplanes));
+	try {
+		const res = await axios.post("/airplanes/find", {...data});
+		const airplanes = await res.data;
+		dispatch(getFoundAirplanes(airplanes));
+	} 
+	catch (err) {
+		// eslint-disable-next-line no-console
+		console.log(err);
+	}
 };
 
 export const getFoundAirplanes = (airplanes) => {
 	return {
 		type: actionTypes.FIND_AIRPLANES,
-		payload:[...airplanes]
+		payload:airplanes
 	};
 };
