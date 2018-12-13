0cfb1b2b912e926e567621a9ca3d03d1e6fb0b58
maannnne <mane.poghosian@gmail.com>
2018-12-13 23:52:55 +0400

    Add integration test for Sign Up page

---
+++ client/src/pages/SignupPageObject.js
@@ -0,0 +1,31 @@
+const BasePageObject = require('./BasePageObject.js').default;
+
+module.exports['default'] = class SignupPageObject extends BasePageObject{
+    constructor(selector) {
+        super();
+        this.selector = selector;
+    }
+
+    fillInName(){
+        this.setValue(this.selector.nameInput, "Mane Poghosian");
+    }
+    fillInEmail(){
+        this.setValue(this.selector.emailInput, "tester1@gmail.com");
+    }
+    fillInPassword(){
+        this.setValue(this.selector.passwordInput, "password");
+    }
+    fillInPassConf(){
+        this.setValue(this.selector.passConfInput, "password");
+    }
+    clickRegisterButton(){
+        console.log("yaaa yaaaa yaaaaaaaaa");
+        this.click(this.selector.registerButton);
+    }
+    getHeaderText(){
+        return browser.element(this.selector.registrationH1).getText();
+    }
+    waitUntilH1Exists(){
+        browser.waitForExist(this.selector.registrationH1, 10000);
+    }
+}
