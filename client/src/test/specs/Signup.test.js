46e48544ce73f54b7aac512fd6463bc06a333647
maannnne <mane.poghosian@gmail.com>
2018-12-13 23:52:55 +0400

    Add integration test for Sign Up page

--- client/src/test/specs/Signup.test.js
+++ client/src/test/specs/Signup.test.js
@@ -1,13 +1,23 @@
 const assert = require('assert');
 const HomePageObject = require('../../pages/HomePageObject').default;
 const HomeConfig = require('../../configs/HomeConfig.json');
-
-
+const SignupPageObject = require('../../pages/SignupPageObject').default;
+const SignupConfig = require('../../configs/SignupConfig.json');
 
 describe('Sign up', () => {
   let homePageObject = new HomePageObject(HomeConfig);
+  let signupPageObject = new SignupPageObject(SignupConfig);
   it('Sign up functionality', () => {
     homePageObject.navigateToHomePage();
     homePageObject.clickOnSignupButton();
+    let headerText = signupPageObject.getHeaderText();
+    console.log(headerText);
+    signupPageObject.waitUntilH1Exists();
+    assert.equal(headerText, "Registration");
+    signupPageObject.fillInName();
+    signupPageObject.fillInEmail();
+    signupPageObject.fillInPassword();
+    signupPageObject.fillInPassConf();
+    signupPageObject.clickRegisterButton();
   });
 });
