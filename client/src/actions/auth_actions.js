af2086f4c4be3e4c2125faad4afcffc843f78613
Armine Gevorgyan <arminegevorgyan1993@gmail.com>
2018-12-14 20:12:04 +0400

    fix: signin functionality

--- client/src/actions/auth_actions.js
+++ client/src/actions/auth_actions.js
@@ -5,42 +5,29 @@ import Auth from "../Auth";
 import jwtDecode from "jwt-decode";
 
 
-export function setCurrentUser(user) {
-	return {
-		type: actionTypes.SET_CURRENT_USER,
-		payload: user
-	};
-}
+export const setCurrentUser = user => ({
+	type: actionTypes.SET_CURRENT_USER,
+	payload: user
+});
 
-export function logout() {
-	return dispatch => {
-		localStorage.removeItem("jwtToken");
-		setAuthorizationToken(false);
-		dispatch(setCurrentUser({}));
-	};
-}
+export const logout = () => dispatch => {
+	Auth.deauthenticateToken();
+	setAuthorizationToken(false);
+	dispatch(setCurrentUser({}));
+};
 
-export function login(data) {
-	return async dispatch => {
-		try {
-		// data = {
-		// 	"email":"benedict@mail.com",
-		// 	"password":"123456"
-		// };
-			const res = await axios.post("users/login", data);
-			dispatch({ type: actionTypes.AUTHENTICATED });
-			const token = res.data.auth_token;
-
-			console.log(jwtDecode(res.data.auth_token));
-			
-			// Auth.authenticateToken(token);
-			setAuthorizationToken(token);
-			dispatch(setCurrentUser(jwtDecode(token)));
-		} catch(err) {
-			dispatch({
-				type: actionTypes.AUTHENTICATION_ERROR,
-				payload: "Invalid email or password"
-			});
-		}
-	};
-}
+export const login = data => async dispatch => {
+	try {
+		const res = await axios.post("users/login", data);
+		dispatch({ type: actionTypes.AUTHENTICATED });
+		const token = res.data.auth_token;
+		Auth.authenticateToken(token);
+		setAuthorizationToken(token);
+		dispatch(setCurrentUser(jwtDecode(token)));
+	} catch(err) {
+		dispatch({
+			type: actionTypes.AUTHENTICATION_ERROR,
+			payload: "Invalid email or password"
+		});
+	}
+};
