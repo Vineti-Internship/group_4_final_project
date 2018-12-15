a096c0028773860654273a538158d6c15103b94d
arpi <arpiarustamyan21@gmail.com>
2018-12-15 13:12:16 +0400

    feat(action): add lane and airplane action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,6 @@
 export const GET_FLIGHTS = "GET_FLIGHTS";
 export const FIND_AIRPLANES = "FIND_AIRPLANES";
+export const GET_AIRPLANES = "GET_AIRPLANES";
 export const FIND_LANES = "FIND_LANES";
+export const GET_LANES = "GET_LANES";
 export const CREATE_USER = "CREATE_USER";
