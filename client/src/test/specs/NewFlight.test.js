b60ce67c5113e99cb9b85a55dbe98a1ad36830de
maannnne <mane.poghosian@gmail.com>
2018-12-16 00:25:33 +0400

    Add New Flight integration and manual tests

--- client/src/test/specs/NewFlight.test.js
+++ client/src/test/specs/NewFlight.test.js
@@ -8,11 +8,38 @@ const NewFlightConfig = require('../../configs/NewFlightConfig.json');
 describe('Create New Flight', () => {
     let homePageObject = new HomePageObject(HomeConfig);
     let newFlightPageObject= new NewFlightPageObject(NewFlightConfig);
-    it('Create new flight', () => {
+    it('Navigate to + New Flight page', () => {
         homePageObject.navigateToHomePage();
         homePageObject.clickOnNewFlightButton();
         let header = newFlightPageObject.getHeaderText();
         console.log(header);
         assert.equal('Create New Flight', header);
+    });
+    it('Create New Flight', () => {
+        newFlightPageObject.setDestination("New York");
+        newFlightPageObject.setStartTime('2018-12-13T02:00');
+        newFlightPageObject.setDuration("45");
+        newFlightPageObject.setCapacity('104');
+        newFlightPageObject.findAvailAirplane();
+        newFlightPageObject.selectAirplane();
+        let text1 = newFlightPageObject.getCapacity();
+        let cap1 = text1.split(' ')[1];
+        console.log(cap1);
+        if(cap1 > 100){
+            newFlightPageObject.findAvailLane();
+            newFlightPageObject.selectLane();
+            let text2 = newFlightPageObject.getCapacity();
+            let cap2 = text2.split(' ')[1];
+            console.log(cap2);
+            if(cap2 > 100){
+                newFlightPageObject.createFlightButton();
+            }
+            else{
+                assert.fail();
+            }
+        }
+        else{
+            assert.fail();
+        }
     });
 });
