76e29f5170530bfc2bd3ab349394fb45c140e7bb
arpi <arpiarustamyan21@gmail.com>
2018-12-15 13:22:44 +0400

    fix(lane): naming fix

--- client/src/reducers/lanes_reducer.js
+++ client/src/reducers/lanes_reducer.js
@@ -1,17 +1,17 @@
 import * as actionTypes from "../actions/types";
 
 const initialLanesState = {
-	found_lanes:[],
-	all_lanes:[]
+	foundLanes: [],
+	allLanes: []
 };
 
 /*eslint indent: [2, "tab", {"SwitchCase": 1}]*/
 export default (state = initialLanesState, action) => {
 	switch(action.type){
 		case actionTypes.FIND_LANES:
-			return {...state, found_lanes: [...action.payload]};
+			return {...state, foundLanes: [...action.payload]};
 		case actionTypes.GET_LANES:
-			return {...state, all_lanes: [...action.payload]};
+			return {...state, allLanes: [...action.payload]};
 		default:
 			return state;
 	}
