8feccb0531de496884ca3127671e0173a31545b2
arpi <arpiarustamyan21@gmail.com>
2018-12-16 16:23:52 +0400

    fix(airplane_list): update airplane list

--- client/src/components/AirplanesList/index.js
+++ client/src/components/AirplanesList/index.js
@@ -4,11 +4,11 @@ import React from "react";
 import * as airplanesActionCreators from "../../actions/airplanes_actions";
 
 const AirplaneCon = (props) => {
-	return <Airplanes {...props} airplanes={props.airplanes} getAirplanes={props.loadAllAirplanes}/>;
+	return <Airplanes {...props} airplanes={props.airplanes} getAirplanes={props.allAirplanes}/>;
 };
 
 const mapStateToProps = state => ({
-airplanes: state.airplanes.all_airplanes
+airplanes: state.airplanes.allAirplanes
 });
 
 export default connect(mapStateToProps, airplanesActionCreators)(AirplaneCon);
