0564828f9d19a9b754cb2350d36f28b99c611cb0
Armine Gevorgyan <arminegevorgyan1993@gmail.com>
2018-12-16 13:48:56 +0400

    fix: signin messages

--- client/src/components/SignInForm/index.js
+++ client/src/components/SignInForm/index.js
@@ -4,10 +4,14 @@ import React from "react";
 import SignInForm from "./SignInForm";
 import { login } from "../../actions/auth_actions";
 
-const SignInFormCon = props =>
-	<SignInForm 
-		{...props}
-		login={props.login}/>;
+const SignInFormCon = props => 
+		<SignInForm 
+			{...props}
+			login={props.login}
+			error={props.error}/>
 
+const mapStateToProps = state => ({
+	error:state.auth.error
+});		
 
-export default connect(null, {login})(SignInFormCon);
+export default connect(mapStateToProps, {login})(SignInFormCon);
