bc50bfa54d62fb4a187a0bfbee70d5ee9bd883f8
arpi <arpiarustamyan21@gmail.com>
2018-12-17 07:00:34 +0400

    feat(airplane): add airlines to new airplane form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import FlightsReducer from "./flights_reducer";
 import LanesReducer from "./lanes_reducer";
 import AirplanesReducer from "./airplanes_reducer";
+import AirlinesReducer from "./airlines_reducer";
 import UsersReducer from "./users_reducer";
 import auth from "./auth_reducer";
 import TicketReducer from "./ticket_reducer";
@@ -10,7 +11,8 @@ import LaneMaxCountReducer from "./lane_max_count_reducer"
 export default combineReducers({
 	flights: FlightsReducer,
 	lanes: LanesReducer,
-	airplanes: AirplanesReducer,
+  airplanes: AirplanesReducer,
+  airlines: AirlinesReducer,
 	users: UsersReducer,
 	auth,
   tickets: TicketReducer,
